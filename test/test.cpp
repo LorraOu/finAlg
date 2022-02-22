ef27844a7afe7d31198de670c44d97f51d91e594
歐雅婷 <lorraou@Moonstone.local>
2022-02-22 14:47:55 +0800

    20220222 course 02

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    string s = "hello world";
+    printf("%c======>testing for printf",s);
+    return 0;
+}
